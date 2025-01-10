3c6f1be0890d1c32790b7e9533f516f229497d9f
xbNkY <danny@bonky.dev>
2025-01-10 02:34:32 +0100

    Major changes
    - Used different source code
    - Added background
    - Transparent playing field background
    - Transparent background blocks in text
    - Instant downplace control added

---
+++ main.py
@@ -0,0 +1,74 @@
+from settings import *
+from tetris import Tetris, Text
+import sys
+import pathlib
+
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption('Tetris')
+        self.screen = pg.display.set_mode(WIN_RES)
+        self.clock = pg.time.Clock()
+        self.set_timer()
+        self.images = self.load_images()
+        self.bg_image = self.load_background()
+        self.tetris = Tetris(self)
+        self.text = Text(self)
+
+    def load_images(self):
+        files = [item for item in pathlib.Path(SPRITE_DIR_PATH).rglob('*.png') if item.is_file()]
+        images = [pg.image.load(file).convert_alpha() for file in files]
+        images = [pg.transform.scale(image, (TILE_SIZE, TILE_SIZE)) for image in images]
+        return images
+
+    def set_timer(self):
+        self.user_event = pg.USEREVENT + 0
+        self.fast_user_event = pg.USEREVENT + 1
+        self.anim_trigger = False
+        self.fast_anim_trigger = False
+        pg.time.set_timer(self.user_event, ANIM_TIME_INTERVAL)
+        pg.time.set_timer(self.fast_user_event, FAST_ANIM_TIME_INTERVAL)
+
+    def update(self):
+        self.tetris.update()
+        self.clock.tick(60  )
+
+    def draw(self):
+        self.screen.blit(self.bg_image, (0, 0))
+        # Create transparent surface for game field
+        field_surface = pg.Surface(FIELD_RES).convert_alpha()
+        field_surface.fill(FIELD_COLOR)  # Using the transparent color from settings
+        self.screen.blit(field_surface, (0, 0))
+        self.tetris.draw()
+        self.text.draw()
+        pg.display.flip()
+
+    def check_events(self):
+        self.anim_trigger = False
+        self.fast_anim_trigger = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                self.tetris.control(pressed_key=event.key)
+            elif event.type == self.user_event:
+                self.anim_trigger = True
+            elif event.type == self.fast_user_event:
+                self.fast_anim_trigger = True
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+    def load_background(self):
+        bg = pg.image.load(BG_IMAGE_PATH).convert()
+        return pg.transform.scale(bg, WIN_RES)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
